b0ca00b02b5073c7c096e0e06ea2f08c3f20cbe9
AugustoCPinheiro <augustocepinheiro@gmail.com>
2020-06-28 18:28:15 -0300

    adds image values normalization and label truncation

--- classification.py
+++ classification.py
@@ -9,8 +9,7 @@ Y_test = []
 print("lendo arq")
 with open("fall_dataset", 'rb') as file:
     x_train, x_test, Y_train, Y_test = np.load(file, allow_pickle=True)
-
-
+print(Y_test)
 print("leu")
 # x_train, x_test = x_train / 255.0, x_test / 255.0
 x_train = np.array(x_train)
@@ -18,7 +17,9 @@ x_test = np.array(x_test)
 Y_train = np.array(Y_train)
 Y_test = np.array(Y_test)
 
-    
+x_train = x_train / 255
+x_test = x_test / 255
+
 inp = tf.keras.layers.Input(shape=(227,227,3))
 layer = tf.keras.layers.Conv2D(3, (11,11), strides=(4,4), activation="relu")(inp)
 layer = tf.keras.layers.MaxPooling2D((3 ,3), strides=(2,2))(layer)
@@ -37,37 +38,8 @@ model = tf.keras.Model(inputs=inp, outputs=output)
 model.summary()
 model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["acc"])
 
-model.fit(x_train, Y_train, batch_size= 2, epochs=20)
+model.fit(x_train, Y_train, epochs=20)
 
-model.evaluate(x_test, Y_test)
+results = model.evaluate(x_test, Y_test)
 
-# # TensorFlow e tf.keras
-# import tensorflow as tf
-# from tensorflow import keras
-
-# # Librariesauxiliares
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# print(tf.__version__)
-
-# fashion_mnist = keras.datasets.fashion_mnist
-
-# (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-# class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-# train_images = train_images / 255.0
-
-# test_images = test_images / 255.0
-
-# model = keras.Sequential([
-#     keras.layers.Flatten(input_shape=(28, 28)),
-#     keras.layers.Dense(128, activation='relu'),
-#     keras.layers.Dense(10, activation='softmax')
-# ])
-
-# model.compile(optimizer='adam',
-#               loss='sparse_categorical_crossentropy',
-#               metrics=['accuracy'])
-# model.fit(train_images, train_labels, epochs=10)
+print(results)
--- dataset_creation.py
+++ dataset_creation.py
@@ -4,7 +4,7 @@ import numpy as np
 
 image_paths = []
 image_labels = []
-root_directory = "./fall/"
+root_directory = "./dataset/"
 
 def create_paths(image_data):
     image_num = int(image_data[1])
@@ -15,8 +15,12 @@ def create_paths(image_data):
         image_pre_nums = "0"
     
     return root_directory + image_data[0] +"-cam0-rgb/" + image_data[0] + "-cam0-rgb-"+ image_pre_nums + image_data[1] +".png"
-    
-with open("./fall/features/urfall-cam0-adls.csv") as file: 
+def truncate(value):
+    if(value < 0):
+        return 0
+    return value
+
+with open("./"+root_directory+"/features/urfall-cam0-adls.csv") as file: 
     data = file.read()
     filtered = list(filter(lambda x:  x, data.split("\n")))
     mapped = list(filter(lambda x: int(x[2]) != 0,map(lambda x: [x.split(',')[0],x.split(',')[1], x.split(',')[2]], filtered)))
@@ -34,11 +38,11 @@ for index, x in enumerate(image_paths):
     image_array = np.array(Image.open(x).resize((227,227)))
     if(counter == 4):
         x_test.append(image_array)
-        Y_test.append(image_labels[index])
+        Y_test.append(truncate(image_labels[index]))
         counter=1
     else:
         x_train.append(image_array)
-        Y_train.append(image_labels[index])
+        Y_train.append(truncate(image_labels[index]))
         counter = counter + 1
 
 print(np.shape(x_test))
